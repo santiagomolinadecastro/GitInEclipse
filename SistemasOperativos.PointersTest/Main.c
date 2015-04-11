e52887c083c6f710107763a596eb7395c7950903
utnso <utnso@utnso40>
2015-04-11 19:30:36 -0300

    This is my firs commit in Github with Eclipse.

---
+++ SistemasOperativos.PointersTest/Main.c
@@ -0,0 +1,17 @@
+/*
+ * Main.c
+ *
+ *  Created on: 11/4/2015
+ *      Author: utnso
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (){
+
+	char * name = strdup("Santiago");
+
+	printf("Name = %c",*name);
+
+}
